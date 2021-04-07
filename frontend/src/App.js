6bc7f2416a9ad7d00692fdbea8884d5f23f71480
Andrew Alteri <andrewsalteri@gmail.com>
2021-04-07 13:20:15 -0500

    Created functions that render complete and incomplete tabs.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { render } from "react-dom";
 
 const toeatItems = [
@@ -15,23 +15,56 @@ const toeatItems = [
     completed: false,
   },
   {
-    id: 3, 
+    id: 3,
     title: "Quesadilla",
     description: "From Chipotle",
     completed: true,
-  }
+  },
 ];
 
 class App extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewCompleted: false,
+      toeatList: toeatItems,
+    };
+  }
 
-render() {
-  return (
-    <div>
-  
-    </div>
-  );
-}
+  displayCompleted = (status) => {
+    if (status) {
+      return this.setState({ viewCompleted: true });
+    }
+    return this.setState({ viewCompleted: false });
+  };
+
+  renderTabList = () => {
+    return (
+      <div className="nav nav-tabs">
+        <span
+          className={this.state.viewCompleted ? "nav-link active" : "nav-link"}
+          onClick={() => this.displayCompleted(true)}
+        >
+          Complete
+        </span>
+        <span
+          className={this.state.viewCompleted ? "nav-link" : "nav-link active"}
+          onClick={() => this.displayCompleted(false)}
+        >
+          Incomplete
+        </span>
+      </div>
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        {this.renderTabList()}
+        <ul className="list-group list-group-flush border-top-0"></ul>
+      </div>
+    );
+  }
 }
 
-export default App; 
+export default App;
