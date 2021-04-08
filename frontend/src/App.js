675f2e62602d0549fd92820fbd0d21b7948b0906
Andrew Alteri <andrewsalteri@gmail.com>
2021-04-08 17:38:06 -0500

    Created renderItems arrow function that renders items with attached edit and delete buttons. Also added an Add task button.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { render } from "react-dom";
+// import { render } from "react-dom";
 
 const toeatItems = [
   {
@@ -57,12 +57,53 @@ class App extends Component {
     );
   };
 
+  renderItems = () => {
+    const { viewCompleted } = this.state;
+    const newItems = this.state.toeatList.filter(
+      (item) => item.completed === viewCompleted
+    );
+
+    return newItems.map((item) => (
+      <li
+        key={item.id}
+        className="list-group-item d-flex justify-content-between align-items-center"
+      >
+        <span
+          className={`toeat-title mr-2 ${
+            this.state.viewCompleted ? "completed-todo" : ""
+          }`}
+          title={item.description}
+        >
+          {item.title}
+        </span>
+        <span>
+          <button className="btn btn-secondary mr-2">Edit</button>
+          <button className="btn btn-danger">Delete</button>
+        </span>
+      </li>
+    ));
+  };
+
   render() {
     return (
-      <div>
-        {this.renderTabList()}
-        <ul className="list-group list-group-flush border-top-0"></ul>
-      </div>
+      <main className="container">
+        <h1 className="text-white text-uppercase text-center my-4">
+          Toeat app
+        </h1>
+        <div className="row">
+          <div className="col-md-6 col-sm-10 mx-auto p-0">
+            <div className="card p-3">
+              <div className="mb-4">
+                <button className="btn btn-primary">Add task</button>
+              </div>
+              {this.renderTabList()}
+              <ul className="list-group list-group-flush border-top-0">
+                {this.renderItems()}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </main>
     );
   }
 }
